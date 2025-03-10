ed5d6a34659b9fc99cccb0ea094900a7756422d4
Sándor Zsombor <o58vps@inf.elte.hu>
2025-03-10 21:03:14 +0100

    Moved MainWindow to View and added ViewModel class

--- Safari/SafariView/App.xaml.cs
+++ Safari/SafariView/App.xaml.cs
@@ -10,6 +10,29 @@ namespace SafariView
     public partial class App : Application
     {
 
+        #region Private fields
+        private MainWindow window;
+        private ViewModel.ViewModel viewModel;
+        #endregion
+
+        #region Constructor
+        public App()
+        {
+            Startup += new StartupEventHandler(AppStartUp);
+        }
+        #endregion
+
+        #region Startup method
+        public void AppStartUp(object? sender, StartupEventArgs e)
+        {
+            viewModel = new ViewModel.ViewModel();
+
+            window = new MainWindow();
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
+        #endregion
     }
 
 }
