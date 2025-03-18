62437f7846dac5dd200abfbe37e1437ca32c26f1
Ferenczy Kata <ferenczy.kata73@gmail.com>
2025-03-18 22:33:10 +0100

    Creating lobby window

--- Safari/SafariView/ViewModel/ViewModel.cs
+++ Safari/SafariView/ViewModel/ViewModel.cs
@@ -11,8 +11,21 @@ namespace SafariView.ViewModel
 {
     public class ViewModel
     {
+        private Model _model = null!;
 
-  
+        public DelegateCommand ExitGameCommand { get; private set; }
+        public event EventHandler? ExitGame;
 
+        public ViewModel(Model model)
+        {
+            _model = model;
+
+            ExitGameCommand = new DelegateCommand(param => OnGameExit());
+        }
+
+        private void OnGameExit()
+        {
+            ExitGame?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
