1697e3d0e214061144c7a2cefe753d96d37d87bd
Sándor Zsombor <o58vps@inf.elte.hu>
2025-03-23 23:33:52 +0100

    Fix merge conflict of 7c07b6e81

--- Safari/SafariView/ViewModel/EntityRender.cs
+++ Safari/SafariView/ViewModel/EntityRender.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace SafariView.ViewModel
 {
     public class EntityRender : ViewModelBase
     {
-        private int x;
-        private int y;
+        private Thickness margin;
         private Brush col;
 
-        public int I { get { return x; } private set { x = value; OnPropertyChanged(); } }
-        public int J { get { return y; } private set { y = value; OnPropertyChanged(); } }
+        public EntityRender(int x, int y, Brush tex)
+        {
+            Margin = new Thickness(x, y, 0, 0);
+            Col = tex;
+        }
+
+        public Thickness Margin { get { return margin; } private set { margin = value; OnPropertyChanged(); } }
         public Brush Col { get { return col; } private set { col = value; OnPropertyChanged(); } }
     }
 }
---
+++ Safari/SafariView/ViewModel/TileCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SafariView.ViewModel
+{
+    public class TileCanvas : Canvas
+    {
+        
+        public List<TileRender>? tiles;
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+            if (tiles == null) return;
+            foreach (TileRender tr in tiles!)
+            {
+                dc.DrawRectangle(tr.Texture, null, tr.Rectangle);
+            }
+        }
+    }
+}
