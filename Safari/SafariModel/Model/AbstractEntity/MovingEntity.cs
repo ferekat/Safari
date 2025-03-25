9f1e32bb1681f9849b9b95f9c8fa28a19ee170b4
Sándor Zsombor <o58vps@inf.elte.hu>
2025-03-25 18:19:51 +0100

    Add entity movement

--- Safari/SafariModel/Model/AbstractEntity/MovingEntity.cs
+++ Safari/SafariModel/Model/AbstractEntity/MovingEntity.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SafariModel.Model.Tiles;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,28 +11,86 @@ namespace SafariModel.Model.AbstractEntity
 {
     public class MovingEntity : Entity
     {
-        protected int speed;
+        private Queue<Point> targetPoints;
+        public Point currentTarget;
+        public Vector2 movementVector;
+        private float subX;
+        private float subY;
+
+        protected float speed;
         protected int range;
         protected bool isMoving;
 
-        public int Speed { get { return speed; } }
+        public float Speed { get { return speed; } }
         public int Range { get { return range; } }
         public bool IsMoving { get { return isMoving; } }
         protected MovingEntity(int x, int y) : base(x, y)
         {
-
+            targetPoints = new Queue<Point>();
+            movementVector = new Vector2();
+            subX = 0;
+            subY = 0;
+            isMoving = false;
+            speed = 5.5F;
         }
         public void SetTarget(Point p)
         {
+            targetPoints.Clear();
+            //Calculate route with pathfinding algorithm and put resulting points into targetPoints
+            currentTarget = p;
 
+            isMoving = true;
+
+            CalculateMovementVector();
         }
-        public void MoveTowardsTarget()
-        {
 
+        private void CalculateMovementVector()
+        {
+            currentTarget.X = Math.Clamp(currentTarget.X,0, Model.MAPSIZE * Tile.TILESIZE);
+            currentTarget.Y = Math.Clamp(currentTarget.Y, 0, Model.MAPSIZE * Tile.TILESIZE);
+
+            movementVector.X = currentTarget.X - this.X;
+            movementVector.Y = currentTarget.Y - this.Y;
+
+            movementVector = Vector2.Multiply(movementVector, speed / movementVector.Length());
+        }
+
+        private void MoveTowardsTarget()
+        {
+            subX += movementVector.X;
+            subY += movementVector.Y;
+            int wholeX = (int)subX;
+            int wholeY = (int)subY;
+            subX -= float.Floor(subX);
+            subY -= float.Floor(subY);
+            this.x += wholeX;
+            this.y += wholeY;
+
+            if (Math.Sqrt(Math.Pow(currentTarget.X - this.x, 2) + Math.Pow(currentTarget.Y - this.y, 2)) < movementVector.Length()) //In range of target point
+            {
+                this.x = currentTarget.X;
+                this.y = currentTarget.Y;
+
+                if (targetPoints.Count > 0)
+                {
+                    currentTarget = targetPoints.Dequeue();
+                    CalculateMovementVector();
+                }
+                else //reached initial target
+                {
+                    isMoving = false;
+                }
+            }
         }
         private bool CalculateRoute()
         {
             return false;
+        }
+
+        public override void EntityTick()
+        {
+            if(isMoving) MoveTowardsTarget();
+            
         }
     }
 }
