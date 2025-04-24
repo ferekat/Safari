539a4ff65bd1369dec6ba8de5a2871ed8a22fc97
Sándor Zsombor <o58vps@inf.elte.hu>
2025-04-24 20:43:34 +0200

    Add test for getting an entity's visible entities

--- Safari/SafariTest/SafariTest.cs
+++ Safari/SafariTest/SafariTest.cs
@@ -1,12 +1,30 @@
-﻿namespace SafariTest
+﻿using SafariModel.Model;
+using SafariModel.Model.AbstractEntity;
+using SafariModel.Model.InstanceEntity;
+using SafariModel.Model.Utils;
+
+namespace SafariTest
 {
     [TestClass]
     public sealed class SafariTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEntityRange()
         {
-            Assert.AreEqual(1, 1);
+            EntityHandler handler = new EntityHandler();
+            Entity.RegisterHandler(handler);
+            //Range: 200 pixels
+            Gazelle g1 = new Gazelle(100,200);
+            Gazelle g2 = new Gazelle(200, 200);
+            Gazelle g3 = new Gazelle(100, 100);
+            //This entity is out of g1's range
+            Gazelle g4 = new Gazelle(1000, 1000);
+            handler.LoadEntity(g1);
+            handler.LoadEntity(g2);
+            handler.LoadEntity(g3);
+            handler.LoadEntity(g4);
+
+            Assert.AreEqual(2, g1.GetEntitiesInRange().Count);
         }
     }
 }
